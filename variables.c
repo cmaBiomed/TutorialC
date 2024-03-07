9ffca517d539e488d69712ee598818e38c9fe6fb
cmaBiomed <carlosmadridtec@gmail.com>
2024-03-07 16:53:34 +0100

    variables

---
+++ variables.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int x;
+    x = 100;
+    int y = 2;
+    int edad = 21;
+    float media = 2.66;
+    char proyecto = 'x';
+    //String
+    char pollo[] = "POLLOS";
+    // Poner tipos de datos en consola
+    printf("tienes %d %s\n", x, pollo);
+    return 0;
+}
